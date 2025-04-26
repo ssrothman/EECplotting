cd9945673cd797b5f767152139c634b6cbfbea47
Simon Rothman <srothman@mit.edu>
2025-04-26 18:18:08 -0400

    sumwts

--- datasets.py
+++ datasets.py
@@ -3,6 +3,7 @@ import pickle
 import os.path
 
 basedir = '/ceph/submit/data/user/s/srothman/EEC/'
+unf_basedir = '/home/submit/srothman/work/EEC/EECunfold/data'
 
 def get_dataset(runtag, tag, skimmer, objsyst, whichobj):
     thepath = os.path.join(basedir, runtag, tag, skimmer)
@@ -34,6 +35,40 @@ def get_dataset(runtag, tag, skimmer, objsyst, whichobj):
     print("The path is: %s" % thepath)
 
     return ds.dataset(thepath, format="parquet")
+
+def get_counts(runtag, tag):
+    thepath = os.path.join(basedir, runtag, tag, 'Count')
+
+    print(thepath)
+    subpaths = os.scandir(thepath)
+    options = []
+    for subpath in subpaths:
+        options.append(subpath.name)
+
+    if (len(options) == 1):
+        print()
+        print("Only one option found: %s" % options[0])
+        print("No user input needed :D")
+        print()
+        thepath = os.path.join(thepath, options[0])
+    else:
+        print()
+        print("Multiple options found:")
+        for i, option in enumerate(options):
+            print(f"{i}: {option}")
+        print()
+        choice = int(input("Please select a number: "))
+        thepath = os.path.join(thepath, options[choice])
+        print()
+
+    print("The path is: %s" % thepath)
+
+    with open(thepath, 'rb') as f:
+        return pickle.load(f)['num_evt']
+
+def get_unfolded_histogram(name):
+    with open(os.path.join(unf_basedir, name+'.pkl'), 'rb') as f:
+        return pickle.load(f)
 
 def get_pickled_histogram(runtag, tag, skimmer, objsyst, wtsyst, whichobj):
     thepath = os.path.join(basedir, runtag, tag, skimmer)
--- testkin.py
+++ testkin.py
@@ -1,20 +1,69 @@
-from datasets import get_dataset
+from datasets import get_dataset, get_counts
 import plotters.kin
 
-which = 'reco'
-df_pythia = get_dataset('Apr_01_2025', 'Pythia_inclusive', 'EECres4tee', 'nominal', which)
-df_herwig = get_dataset('Apr_01_2025', 'Herwig_inclusive', 'EECres4tee', 'nominal', which)
-#df_dataA = get_dataset('Apr_01_2025', 'DATA_2018A', 'EECres4tee', 'nominal', which)
-#df_dataB = get_dataset('Apr_01_2025', 'DATA_2018B', 'EECres4tee', 'nominal', which)
-#df_dataC = get_dataset('Apr_01_2025', 'DATA_2018C', 'EECres4tee', 'nominal', which)
-df_dataD = get_dataset('Apr_01_2025', 'DATA_2018D', 'EECres4tee', 'nominal', which)
+from importlib import reload
+import numpy as np
+import json
+
+skimmer = 'Kinematics'
+which = 'event'
+
+with open("config.json", 'r') as f:
+    config = json.load(f)
+
+datasets = {}
+for dataset in config['DatasetsMC'].keys():
+    tag = config['DatasetsMC'][dataset]['tag']
+    label = config['DatasetsMC'][dataset]['label']
+    xsec = config['DatasetsMC'][dataset]['xsec']
+
+    df = get_dataset('Apr_23_2025', tag, skimmer, 'nominal', which)
+    count = get_counts('Apr_23_2025', tag)
+    datasets[dataset] = {
+        'df' : df,
+        'label' : label,
+        'xsec' : xsec,
+        'numevts' : count
+    }
 
 plotter = plotters.kin.KinPlotManager()
-plotter.add_MC(df_pythia, 'Pythia')
-plotter.add_MC(df_herwig, 'Herwig')
+
+for dataset in datasets.keys():
+    if 'Pythia' in dataset:
+        plotter.add_MC(**datasets[dataset])
+
+#plotter.add_MC(df_pythia, 'Pythia', 1, count_pythia)
+#plotter.add_MC(df_herwig, "Herwig", 1, count_herwig)
+#plotter.add_MC(df_HT0to70, 'Pythia HT0to70', 1, count_HT0to70)
+#plotter.add_MC(df_HT70to70, 'Pythia HT70to100', 1, count_HT70to100)
+#plotter.add_MC(df_HT100to200, 'Pythia HT100to200', 1, count_HT100to200)
+#plotter.add_MC(df_HT200to400, 'Pythia HT200to400', 1, count_HT200to400)
+#plotter.add_MC(df_HT400to600, 'Pythia HT400to600', 1, count_HT400to600)
+#plotter.add_MC(df_HT600to800, 'Pythia HT600to800', 1, count_HT600to800)
+#plotter.add_MC(df_HT800to1200, 'Pythia HT800to1200', 1, count_HT800to1200)
+#plotter.add_MC(df_HT1200to2500, 'Pythia HT1200to2500', 1, count_HT1200to2500)
+#plotter.add_MC(df_HT2500toInf, 'Pythia HT2500toInf', 1, count_HT2500toInf)
+#plotter.add_MC(df_herwig, 'Herwig/2', 0.5, count_herwig)
+#plotter.add_MC(df_herwig, 'Herwig*2', 2.0, count_herwig)
 #plotter.add_data(df_dataA)
 #plotter.add_data(df_dataB)
 #plotter.add_data(df_dataC)
-plotter.add_data(df_dataD)
+#plotter.add_data(df_dataD)
 
-plotter.plot_variable('pt')
+plotter.toggle_show(True)
+#plotter.setup_savefig('test_kin/parts')
+
+#from tqdm import tqdm
+#
+#for key in df_dataD.schema:
+#    toplot = key.name
+#    if toplot.startswith('evtwt'):
+#        continue
+#    if toplot in ['CHSpt', 'CHSeta', 'CHSphi']:
+#        continue
+#
+#    print("plotting", toplot)
+#    plotter.plot_variable(toplot)
+#
+#if which == 'part':
+#    plotter.plot_variable('pt_over_jetPt')
