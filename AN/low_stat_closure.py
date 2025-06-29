0dab5a7dc24f067b1d3919d5289e8bd6838dacc4
Simon Rothman <srothman@mit.edu>
2025-06-29 13:48:16 -0400

    ioutil from unfolding repo

--- AN/low_stat_closure.py
+++ AN/low_stat_closure.py
@@ -6,71 +6,81 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 
-with open("/ceph/submit/data/group/cms/store/user/srothman/EEC/Apr_23_2025/Herwig_inclusive/EECres4tee/UNFOLDED/RECO_boot2000/LOSS_2d_Apr_23_2025_Pythia_inclusive_boot2000/Hunf.pkl", 'rb') as f:
+with open("/ceph/submit/data/group/cms/store/user/srothman/EEC/Apr_23_2025/Herwig_inclusive/EECres4tee/UNFOLDED/RECO_boot-1/LOSS_1d_Apr_23_2025_Pythia_inclusive_boot-1/Hunf.pkl", 'rb') as f:
     Hunf_herwig = pickle.load(f)
 
-with open("/ceph/submit/data/group/cms/store/user/srothman/EEC/Apr_23_2025/Herwig_inclusive/EECres4tee/UNFOLDED/RECO_boot2000/LOSS_2d_Apr_23_2025_Pythia_inclusive_boot2000/Hunf_statonly.pkl", 'rb') as f:
+with open("/ceph/submit/data/group/cms/store/user/srothman/EEC/Apr_23_2025/Herwig_inclusive/EECres4tee/UNFOLDED/RECO_boot-1/LOSS_1d_Apr_23_2025_Pythia_inclusive_boot-1/Hunf_statonly.pkl", 'rb') as f:
     Hunf_herwig_statonly = pickle.load(f)
 
-with open("/ceph/submit/data/group/cms/store/user/srothman/EEC/Apr_23_2025/Pythia_inclusive/EECres4tee/UNFOLDED/RECO_boot2000_2stat1/LOSS_2d_Apr_23_2025_Pythia_inclusive_boot2000_2stat0/Hunf.pkl", 'rb') as f:
+with open("/ceph/submit/data/group/cms/store/user/srothman/EEC/Apr_23_2025/Pythia_inclusive/EECres4tee/UNFOLDED/RECO_boot-1_2stat1/LOSS_1d_Apr_23_2025_Pythia_inclusive_boot-1_2stat0/Hunf.pkl", 'rb') as f:
     Hunf_Spythia = pickle.load(f)
 
-with open("/ceph/submit/data/group/cms/store/user/srothman/EEC/Apr_23_2025/Pythia_inclusive/EECres4tee/UNFOLDED/RECO_boot2000_2stat1/LOSS_2d_Apr_23_2025_Pythia_inclusive_boot2000_2stat0/Hunf_statonly.pkl", 'rb') as f:
+with open("/ceph/submit/data/group/cms/store/user/srothman/EEC/Apr_23_2025/Pythia_inclusive/EECres4tee/UNFOLDED/RECO_boot-1_2stat1/LOSS_1d_Apr_23_2025_Pythia_inclusive_boot-1_2stat0/Hunf_statonly.pkl", 'rb') as f:
     Hunf_Spythia_statonly = pickle.load(f)
 
-with open("/ceph/submit/data/group/cms/store/user/srothman/EEC/Apr_23_2025/Pythia_inclusive/EECres4tee/UNFOLDED/RECO_boot2000/LOSS_2d_Apr_23_2025_Pythia_inclusive_boot2000/Hunf.pkl", 'rb') as f:
+with open("/ceph/submit/data/group/cms/store/user/srothman/EEC/Apr_23_2025/Pythia_inclusive/EECres4tee/UNFOLDED/RECO_boot-1/LOSS_1d_Apr_23_2025_Pythia_inclusive_boot-1/Hunf.pkl", 'rb') as f:
      Hunf_pythia = pickle.load(f)
 
-with open("/ceph/submit/data/group/cms/store/user/srothman/EEC/Apr_23_2025/Pythia_inclusive/EECres4tee/UNFOLDED/RECO_boot2000/LOSS_2d_Apr_23_2025_Pythia_inclusive_boot2000/Hunf_statonly.pkl", 'rb') as f:
+with open("/ceph/submit/data/group/cms/store/user/srothman/EEC/Apr_23_2025/Pythia_inclusive/EECres4tee/UNFOLDED/RECO_boot-1/LOSS_1d_Apr_23_2025_Pythia_inclusive_boot-1/Hunf_statonly.pkl", 'rb') as f:
     Hunf_pythia_statonly = pickle.load(f)
 
-with open("/ceph/submit/data/group/cms/store/user/srothman/EEC/Apr_23_2025/Herwig_inclusive/EECres4tee/UNFOLDED/RECO_boot2000/LOSS_2d_Apr_23_2025_Pythia_inclusive_boot2000/minimization_result.pkl", 'rb') as f:
+with open("/ceph/submit/data/group/cms/store/user/srothman/EEC/Apr_23_2025/Herwig_inclusive/EECres4tee/UNFOLDED/RECO_boot-1/LOSS_1d_Apr_23_2025_Pythia_inclusive_boot-1/minimization_result.pkl", 'rb') as f:
     res_herwig = pickle.load(f)[0]
 
-with open("/ceph/submit/data/group/cms/store/user/srothman/EEC/Apr_23_2025/Pythia_inclusive/EECres4tee/UNFOLDED/RECO_boot2000_2stat1/LOSS_2d_Apr_23_2025_Pythia_inclusive_boot2000_2stat0/minimization_result.pkl", 'rb') as f:
+with open("/ceph/submit/data/group/cms/store/user/srothman/EEC/Apr_23_2025/Pythia_inclusive/EECres4tee/UNFOLDED/RECO_boot-1_2stat1/LOSS_1d_Apr_23_2025_Pythia_inclusive_boot-1_2stat0/minimization_result.pkl", 'rb') as f:
     res_Spythia = pickle.load(f)[0]
  
-with open("/ceph/submit/data/group/cms/store/user/srothman/EEC/Apr_23_2025/Pythia_inclusive/EECres4tee/UNFOLDED/RECO_boot2000/LOSS_2d_Apr_23_2025_Pythia_inclusive_boot2000/minimization_result.pkl", 'rb') as f:
+with open("/ceph/submit/data/group/cms/store/user/srothman/EEC/Apr_23_2025/Pythia_inclusive/EECres4tee/UNFOLDED/RECO_boot-1/LOSS_1d_Apr_23_2025_Pythia_inclusive_boot-1/minimization_result.pkl", 'rb') as f:
     res_pythia = pickle.load(f)[0]
 
 Hgen_herwig = get_pickled_histogram('Apr_23_2025', 'Herwig_inclusive', 
                                     'EECres4tee', 'nominal', 'nominal',
-                                    'gen', max_nboot=2000)
+                                    'gen',
+                                    reweight=None)
 
-Hgen_Spythia = get_pickled_histogram('Apr_23_2025', 'Herwig_inclusive', 
+Hgen_Spythia = get_pickled_histogram('Apr_23_2025', 'Pythia_inclusive', 
                                     'EECres4tee', 'nominal', 'nominal',
-                                    'gen', max_nboot=2000,
-                                    statN = 2, statK = 1)
+                                    'gen',
+                                    statN = 2, statK = 1,
+                                     reweight=None)
 
-Hgen_pythia = get_pickled_histogram('Apr_23_2025', 'Herwig_inclusive', 
+Hgen_pythia = get_pickled_histogram('Apr_23_2025', 'Pythia_inclusive', 
                                     'EECres4tee', 'nominal', 'nominal',
-                                    'gen', max_nboot=2000)
+                                    'gen',
+                                    reweight=None)
 
-os.makedirs('AN/low_stat_closure', exist_ok=True)
-os.makedirs('AN/low_stat_closure/perfect', exist_ok=True)
-os.makedirs('AN/low_stat_closure/stat', exist_ok=True)
-os.makedirs('AN/low_stat_closure/model', exist_ok=True)
+os.makedirs('AN/low_stat_closure_1d', exist_ok=True)
+os.makedirs('AN/low_stat_closure_1d/perfect', exist_ok=True)
+os.makedirs('AN/low_stat_closure_1d/stat', exist_ok=True)
+os.makedirs('AN/low_stat_closure_1d/model', exist_ok=True)
 
 plotters.res4.plot_correlations(res_herwig.invhess, 
-                                savefig='AN/low_stat_closure/model/correlations.png')
+                                savefig='AN/low_stat_closure_1d/model/correlations.png')
 plotters.res4.plot_correlations(res_Spythia.invhess,
-                                savefig='AN/low_stat_closure/stat/correlations.png')
+                                savefig='AN/low_stat_closure_1d/stat/correlations.png')
 plotters.res4.plot_correlations(res_pythia.invhess,
-                                savefig='AN/low_stat_closure/perfect/correlations.png')
+                                savefig='AN/low_stat_closure_1d/perfect/correlations.png')
+
+plotters.res4.plot_correlations(res_herwig.invhess[:7875, :7875], 
+                                savefig='AN/low_stat_closure_1d/model/correlations_zoom.png')
+plotters.res4.plot_correlations(res_Spythia.invhess[:7875, :7875],
+                                savefig='AN/low_stat_closure_1d/stat/correlations_zoom.png')
+plotters.res4.plot_correlations(res_pythia.invhess[:7875, :7875],
+                                savefig='AN/low_stat_closure_1d/perfect/correlations_zoom.png')
 
 plotters.res4.plot_pulls(res_herwig, 
-                         savefig='AN/low_stat_closure/model/pulls.png')
+                         savefig='AN/low_stat_closure_1d/model/pulls.png')
 plotters.res4.plot_pulls(res_Spythia,
-                         savefig='AN/low_stat_closure/stat/pulls.png')
+                         savefig='AN/low_stat_closure_1d/stat/pulls.png')
 plotters.res4.plot_pulls(res_pythia,
-                         savefig='AN/low_stat_closure/perfect/pulls.png')
+                         savefig='AN/low_stat_closure_1d/perfect/pulls.png')
 
 plotters.res4.plot_named_pulls(res_herwig,
-                               savefig='AN/low_stat_closure/model/named_pulls.png')
+                               savefig='AN/low_stat_closure_1d/model/named_pulls.png')
 plotters.res4.plot_named_pulls(res_Spythia,
-                               savefig='AN/low_stat_closure/stat/named_pulls.png')
+                               savefig='AN/low_stat_closure_1d/stat/named_pulls.png')
 plotters.res4.plot_named_pulls(res_pythia,
-                               savefig='AN/low_stat_closure/perfect/named_pulls.png')
+                               savefig='AN/low_stat_closure_1d/perfect/named_pulls.png')
 
 for ptbin in range(5):
     for rcbin in [0, 5, 10]:
@@ -79,18 +89,18 @@ for ptbin in range(5):
                                  "Gen", 
                                  "Unfolded [stat+syst]", 
                                  1, ptbin, r=rcbin, c=rcbin,
-                                 savefig='AN/low_stat_closure/model/UNF')
+                                 savefig='AN/low_stat_closure_1d/model/UNF')
 
         plotters.res4.compare_1d(Hgen_Spythia, 
                                  Hunf_Spythia_statonly, 
                                  "Gen", 
                                  "Unfolded [stat only]", 
                                  1, ptbin, r=rcbin, c=rcbin,
-                                 savefig='AN/low_stat_closure/stat/UNF')
+                                 savefig='AN/low_stat_closure_1d/stat/UNF')
 
         plotters.res4.compare_1d(Hgen_pythia, 
                                  Hunf_pythia_statonly, 
                                  "Gen", 
                                  "Unfolded [stat only]", 
                                  1, ptbin, r=rcbin, c=rcbin,
-                                 savefig='AN/low_stat_closure/perfect/UNF')
+                                 savefig='AN/low_stat_closure_1d/perfect/UNF')
--- testres4.py
+++ testres4.py
@@ -1,35 +1,56 @@
-from datasets import get_pickled_histogram, get_unfolded_histogram, get_pickled_histogram_sum
-import pickle
+import argparse
+
+parser = argparse.ArgumentParser(description='Run the minimizer for EEC reconstruction')
+parser.add_argument('RecoTag', type=str)
+parser.add_argument('RecoSample', type=str)
+parser.add_argument('--reco_nboot', type=int, default=-1)
+parser.add_argument('--reco_statN', type=int, default=-1)
+parser.add_argument('--reco_statK', type=int, default=-1)
+parser.add_argument('--reco_firstN', type=int, default=-1)
+parser.add_argument('--reco_wtsyst', type=str, default='nominal')
+parser.add_argument('--reco_objsyst', type=str, default='nominal')
+
+parser.add_argument('GenTag', type=str)
+parser.add_argument('GenSample', type=str)
+parser.add_argument('--gen_nboot', type=int, default=-1)
+parser.add_argument('--gen_statN', type=int, default=-1)
+parser.add_argument('--gen_statK', type=int, default=-1)
+parser.add_argument('--gen_firstN', type=int, default=-1)
+
+parser.add_argument("Run", type=str)
+
+parser.add_argument('--systlist', type=str, nargs='*',
+                    default=['scale', 'isosf', 'idsf', 'triggersf',
+                             'PU', 'PDF', 'aS', 'PDFaS',
+                             'ISR', 'FSR',
+                             'CH', 'JES', 'JER', 'UNCLUSTERED',
+                             'TRK_EFF'])
+
+parser.add_argument('--help_condition', type=float, default=0.001)
+parser.add_argument('--testcut', action='store_true')
+
+args = parser.parse_args()
+
+import filenames
+import os
+
+reco_folder = filenames.reco_folder(
+    args.RecoTag, args.RecoSample, args.reco_nboot,
+    args.reco_statN, args.reco_statK, args.reco_firstN,
+    args.reco_objsyst, args.reco_wtsyst, args.testcut
+)
+loss_folder = filenames.loss_folder(
+    args.GenTag, args.GenSample, args.gen_nboot,
+    args.gen_statN, args.gen_statK, args.gen_firstN,
+    args.systlist, args.testcut
+)
+loss_name = os.path.basename(loss_folder)
+
+base_folder = os.path.join(reco_folder, loss_name, run)
+
 import plotters.res4
 from importlib import reload
 import numpy as np
 import matplotlib.pyplot as plt
-
-with open("/ceph/submit/data/group/cms/store/user/srothman/EEC/Apr_23_2025/Herwig_inclusive/EECres4tee/UNFOLDED/RECO_boot2000/LOSS_2d_Apr_23_2025_Pythia_inclusive_boot2000/Hunf_statonly.pkl", 'rb') as f:
-    Hunf_herwig_2d_statonly = pickle.load(f)
-
-with open("/ceph/submit/data/group/cms/store/user/srothman/EEC/Apr_23_2025/Herwig_inclusive/EECres4tee/UNFOLDED/RECO_boot2000/LOSS_2d_Apr_23_2025_Pythia_inclusive_boot2000/Hunf.pkl", 'rb') as f:
-    Hunf_herwig_2d = pickle.load(f)
-
-with open("/ceph/submit/data/group/cms/store/user/srothman/EEC/Apr_23_2025/Pythia_inclusive/EECres4tee/UNFOLDED/RECO_boot2000_2stat1/LOSS_2d_Apr_23_2025_Pythia_inclusive_boot2000_2stat0/Hunf.pkl", 'rb') as f:
-    Hunf_pythia_2d = pickle.load(f)
-
-with open("/ceph/submit/data/group/cms/store/user/srothman/EEC/Apr_23_2025/Pythia_inclusive/EECres4tee/UNFOLDED/RECO_boot2000_2stat1/LOSS_2d_Apr_23_2025_Pythia_inclusive_boot2000_2stat0/Hunf_statonly.pkl", 'rb') as f:
-    Hunf_pythia_2d_statonly = pickle.load(f)
-
-with open("/ceph/submit/data/group/cms/store/user/srothman/EEC/Apr_23_2025/Pythia_inclusive/EECres4tee/UNFOLDED/RECO_boot2000_2stat1/LOSS_2d_Apr_23_2025_Pythia_inclusive_boot2000_2stat0/minimization_result.pkl", 'rb') as f:
-    res_pythia = pickle.load(f)[0]
-
-with open("/ceph/submit/data/group/cms/store/user/srothman/EEC/Apr_23_2025/Herwig_inclusive/EECres4tee/UNFOLDED/RECO_boot2000/LOSS_2d_Apr_23_2025_Pythia_inclusive_boot2000/minimization_result.pkl", 'rb') as f:
-    res_herwig = pickle.load(f)[0]
- 
-
-
-Hgen_herwig = get_pickled_histogram('Apr_23_2025', 'Herwig_inclusive', 
-                                    'EECres4tee', 'nominal', 'nominal',
-                                    'gen', max_nboot=2000)
-
-Hgen_pythia = get_pickled_histogram('Apr_23_2025', 'Herwig_inclusive', 
-                                    'EECres4tee', 'nominal', 'nominal',
-                                    'gen', max_nboot=2000,
-                                    statN = 2, statK = 0)
+import datasets
+import ioutil
