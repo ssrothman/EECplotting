bbfec5e7fad2d3ae90c6153f77c3d3f44f9c1a91
Simon Rothman <srothman@mit.edu>
2025-12-19 10:18:34 -0500

    Checkpoint simon_mpl_util

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ pythia4 = smu.ParquetDataset('/ceph/submit/data/group/cms/store/user/srothman/EE
 pythia4.set_label("Pythia inclusive")
 pythia4.set_color("red")
 pythia4.set_xsec(6077.22)
+pythia4.set_key('Pythia')
 import pickle
 with open("/ceph/submit/data/group/cms/store/user/srothman/EEC/Apr_23_2025/Pythia_inclusive/Count/hists_file0to396_MC_nominal.pickle", 'rb') as f:
     pythia4.override_num_events(pickle.load(f)['num_evt'])
@@ -14,6 +15,7 @@ pythia = smu.ParquetDataset('/ceph/submit/data/group/cms/store/user/srothman/EEC
 pythia.set_label("Pythia inclusive x1/3")
 pythia.set_color("blue")
 pythia.set_xsec(6077.22/3)
+pythia.set_key('Pythia')
 import pickle
 with open("/ceph/submit/data/group/cms/store/user/srothman/EEC/Apr_23_2025/Pythia_inclusive/Count/hists_file0to396_MC_nominal.pickle", 'rb') as f:
     pythia.override_num_events(pickle.load(f)['num_evt'])
@@ -22,6 +24,7 @@ pythia2 = smu.ParquetDataset('/ceph/submit/data/group/cms/store/user/srothman/EE
 pythia2.set_label("Pythia inclusive x2/3")
 pythia2.set_color("orange")
 pythia2.set_xsec(6077.22*2/3)
+pythia2.set_key('Pythia')
 import pickle
 with open("/ceph/submit/data/group/cms/store/user/srothman/EEC/Apr_23_2025/Pythia_inclusive/Count/hists_file0to396_MC_nominal.pickle", 'rb') as f:
     pythia2.override_num_events(pickle.load(f)['num_evt'])
@@ -29,11 +32,13 @@ with open("/ceph/submit/data/group/cms/store/user/srothman/EEC/Apr_23_2025/Pythi
 pythia3 = smu.DatasetStack([pythia, pythia2])
 pythia3.set_label("[Stack] Pythia inclusive")
 pythia3.set_color("green")
+pythia3.set_key('Pythia')
 
 data = smu.ParquetDataset('/ceph/submit/data/group/cms/store/user/srothman/EEC/Apr_23_2025/DATA_2018D/Kinematics/hists_file0to768_data/nominal/event/')
 data.set_label("Data")
 data.set_color("black")
 data.set_lumi(31.833472186)
+data.set_key('DATA')
 
 variable = smu.BasicVariable('Zpt')
 cut = smu.NoCut()
@@ -43,12 +48,12 @@ wtMC_alt = smu.BasicVariable("evtwt_FSRUp")
 
 smu.plot_histogram(
     variable, cut,
-    [wtMC, wtMC_alt],
-    [pythia3,pythia3],
+    [wtDT, wtMC],
+    [data,pythia3],
     smu.AutoBinning(),
     ['Nominal', 'FSR Up'],
     density=False,
-    output_path = 'test',
+    output_folder = 'test',
+    output_prefix = None,
     logy=True,
-    logx=True
 )
